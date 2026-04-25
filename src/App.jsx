3d0a225b64a46dbdfce788de0bdead03631f52f6
LynxKandi <maxbuissines@gmail.com>
2026-04-26 00:53:03 +0100

    V2.1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from "./HomePage";
 import SchedulerPage from "./SchedulerPage";
 import LearnPage from "./LearnPage";
 import FeedbackPage from "./FeedbackPage";
 import AuthPage from "./AuthPage";
+
 import AdminRoute from "./Admin/AdminRoute";
 import AdminDashboard from "./Admin/AdminDashboard";
 
@@ -17,14 +19,15 @@ export default function App() {
         <Route path="/learn" element={<LearnPage />} />
         <Route path="/feedback" element={<FeedbackPage />} />
         <Route path="/auth" element={<AuthPage />} />
+
         <Route
-  path="/admin"
-  element={
-    <AdminRoute>
-      <AdminDashboard />
-    </AdminRoute>
-  }
-/>
+          path="/admin"
+          element={
+            <AdminRoute>
+              <AdminDashboard />
+            </AdminRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
